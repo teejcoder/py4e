9b7994a025889f94eebe82ae4ce339cefbfca869
teejcoder <teejcoder@gmail.com>
2026-07-03 15:42:19 +1000

    Loops

--- loops.py
+++ loops.py
@@ -72,3 +72,61 @@ total = 0
 for i in [1,2,3,4,5]:
     total += i
 print('total', total)
+
+# MAXIMUM & MINIMUM LOOPS
+largest = None
+print('before:', largest)
+for intervar in [2,345,67,65,4,3,45,6,7,654,4,564]:
+    if largest is None or intervar > largest:
+        largest = intervar
+    print('Loop', intervar, largest)
+print('LARGEST', largest)
+
+smallest = None
+print('BEFORE:', smallest)
+for i in [3000,300,45,67,65,43,2,3,45,6,78,7,65,43,45,7,5,43,2,1]:
+    if smallest is None or i < smallest:
+        smallest = i
+    print('LOOP>', i, smallest)
+print('SMALLEST:', smallest)
+
+# EXERCISE 1 - Write a program which repeatedly reads integers 
+# until the user enters “done”. 
+# Once “done” is entered, print out the total, count, and average 
+# of the integers. If the user enters anything other than an integer, 
+# detect their mistake using try and except and print an error message 
+# and skip to the next integers.
+# total = 0.0
+# count = 0
+
+while True:
+    sval = input('Enter a number: ')
+    if sval == 'done':
+        break
+    try: 
+        fval = float(sval)
+    except:
+        print('Invalid input')
+        continue
+    total = total + fval
+    count += 1
+print('done!')
+print('total:', total, 'count:', count, "avg:", total/count)
+
+
+# Exercise 2: Write another program that prompts for a 
+# list of numbers as above and at the end prints out 
+# both the maximum and minimum of the numbers instead of the average.
+
+nums = []
+while True:
+    sval = input('enter a number: \n')
+    if sval == 'done':
+        break
+    try:
+        fval = float(sval)
+    except:
+        print('Invalid input')
+        continue
+    nums.append(fval)
+print('MAX:', max(nums), 'MIN: ', min(nums))
